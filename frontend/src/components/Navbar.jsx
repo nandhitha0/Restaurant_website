8bf534c06bae9add5c7c0473104311ea7afb2043
nandhitha0 <nandhu2k05@gmail.com>
2025-07-19 11:48:54 +0530

    update

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -18,7 +18,7 @@ const Navbar = () => {
               MENU
             </li>
             <li className="font-bold text-lg cursor-pointer bg-gradient-to-br from-yellow-800 via-yellow-600 to-yellow-800 bg-clip-text text-transparent  brightness-150 dark:brightness-100 group hover:shadow-lg hover:shadow-yellow-700/60 transition ease-in-out hover:scale-105 p-1 rounded-xl">
-              CONTACT
+              CONTACT--
             </li>
           </ul>
         </div>
